2805b02ed7d6ad2952bc29c7996e649407f6d264
Dan Taylor <dan@logicalgenetics.com>
2019-06-14 14:09:07 +0100

    Some pre-processing of the Activity data

---
+++ tcx2csv.py
@@ -0,0 +1,34 @@
+import xmltodict
+from glob import glob
+
+
+if __name__ == "__main__":
+    FILE_PATTERN = "data/Takeout/Fit/Activities/*.tcx"
+
+    for file in glob(FILE_PATTERN):
+        with open(file) as fd:
+            doc = xmltodict.parse(fd.read(), force_list={'Activity', 'Lap', 'Trackpoint'})
+
+        print(file)
+
+        laps = [lap for activity in doc["TrainingCenterDatabase"]["Activities"]["Activity"] for lap in activity["Lap"]]
+        trackpoints = [tp for lap in laps for tp in lap["Track"]["Trackpoint"]]
+
+        out_filename = file.replace(".tcx", ".csv")
+
+        with open(out_filename, "w") as out_file:
+            out_file.write("time,distance,longitude,latitude")
+
+            for trackpoint in trackpoints:
+                time = trackpoint.get("Time")
+                distance = trackpoint.get("DistanceMeters")
+                position = trackpoint.get("Position")
+                lat = lon = ""
+                if position:
+                    lat = position["LatitudeDegrees"]
+                    lon = position["LongitudeDegrees"]
+
+                out_file.write("{0},{1},{2},{3}\n".format(time, distance, lon, lat))
+
+
+
